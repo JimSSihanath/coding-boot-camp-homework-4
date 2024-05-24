08ce17135dbf3c69cbe22dc3dccb3252fc3d5948
Jim S. Sihanath <j.ssihanath@gmail.com>
2024-05-23 20:01:54 -0600

    Updated script.js JavaScript File

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('blogForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+})
