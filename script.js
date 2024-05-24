c58586264a3ff2154df8b222684157cff556c081
Jim S. Sihanath <j.ssihanath@gmail.com>
2024-05-23 20:08:09 -0600

    Added Comments to script.js File

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Using WebAPI to link to Class blogForm and Add Event Listener for Submit Button.
 document.getElementById('blogForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
+// Define variables for Username, Title, and Content.
     const username = document.getElementById('username').value;
     const title = document.getElementById('title').value;
     const content = document.getElementById('content').value;
