e8250177255591f4f8cbfb1599c7b318ffe363c0
Jim S. Sihanath <j.ssihanath@gmail.com>
2024-05-28 14:34:37 -0600

    Updated blog.js File

--- blog.js
+++ blog.js
@@ -1,3 +1,8 @@
 // File will retreive and display blog posts from the localStorage.
 const posts = JSON.parse(localStorage.getItem('blogPosts')) || [];
 const postsContainer = document.getElementById('posts');
+
+// Iterate through each element in the Post array.
+posts.forEach(post => {
+    const postElement = document.createElement('div');
+})
