e0bfa0c4992e225deaa536458ddd5dce149317c1
Jim S. Sihanath <j.ssihanath@gmail.com>
2024-05-28 14:38:32 -0600

    Updated blog.js File

--- blog.js
+++ blog.js
@@ -5,4 +5,11 @@ const postsContainer = document.getElementById('posts');
 // Iterate through each element in the Post array.
 posts.forEach(post => {
     const postElement = document.createElement('div');
-})
+// Return the HTML.
+    postElement.innerHTML = `
+        <h2>${post.title}</h2>
+        <p><strong>Author:</strong> ${post.username}</p>
+        <p>${post.content}</p>
+        `;
+        postsContainer.appendChild(postElement);
+});
